df9b436615dc26402b504047a130484901ba5842
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-01-20 11:58:02 +0530

    add : counter boiler plate code

--- src/counterApp/App.js
+++ src/counterApp/App.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux'
 
+const INCREMENT = 'INCREMENT'
 
-const CounterApp = ()=>{
-    return <></>
+const incrementValue = () => ({
+  type: INCREMENT,
+})
+
+const initialState = {
+  count: 0,
 }
 
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    }
+  }
+
+  return state
+}
+
+const store = createStore(reducer)
+
+const CounterApp = () => {
+  return (
+    <>
+      <main className="Counter">
+        <p className="count">0</p>
+        <section className="controls">
+          <button>Increment</button>
+          <button>Decrement</button>
+          <button>Reset</button>
+        </section>
+      </main>
+    </>
+  )
+}
 
 export default CounterApp
